08907b16caaa1e34d732e66332ec5f5b4c6363c4
Muskan Gyanani <muskaann.129@gmail.com>
2024-07-12 19:40:37 +0530

    Add: no_of_questions field

--- src/pages/AllQuizzes.jsx
+++ src/pages/AllQuizzes.jsx
@@ -4,9 +4,7 @@ import { MdQuiz } from "react-icons/md";
 const AllQuizzes = () => {
   // const [quizzes, setQuizzes] = React.useState([]);
   // fetch quizzes from the backend
-  // 
   return (
-    
     <div className='mt-10 mx-10 flex gap-4'>
       <div className='flex flex-col gap-3 border border-neutral-400 size-52 rounded-lg p-2'>
         <div className='flex flex-col items-center border py-5 rounded-md bg-teal-700 text-white'>
