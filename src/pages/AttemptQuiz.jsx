917103a8a0207a0b1912c4ff4071d15c57e6cc66
Muskan Gyanani <muskaann.129@gmail.com>
2024-07-13 23:11:00 +0530

    Fix: Score pop up, timer

--- src/pages/AttemptQuiz.jsx
+++ src/pages/AttemptQuiz.jsx
@@ -6,7 +6,7 @@ const AttemptQuiz = () => {
   const [startTimer, setStartTimer] = useState(5);
   const [showTimerModal, setShowTimerModal] = useState(true);
   const [quizName, setQuizName] = useState('');
-  const [time_limit, setTimeLimit] = useState(0);
+  const [timeLimit, setTimeLimit] = useState(0);
   const [minutes, setMinutes] = useState(0);
   const [seconds, setSeconds] = useState(0);
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
@@ -14,6 +14,8 @@ const AttemptQuiz = () => {
   const [selectedOptions, setSelectedOptions] = useState({});
   const [score, setScore] = useState(0);
   const [isSubmitted, setIsSubmitted] = useState(false);
+  const [timeUp, setTimeUp] = useState(false);
+
   let no_of_questions = questions.length;
 
   useEffect(() => {
@@ -33,10 +35,12 @@ const AttemptQuiz = () => {
   }, []);
 
   useEffect(() => {
-    if (minutes === 0 && seconds === 0) {
+    if (!showTimerModal && minutes === 0 && seconds === 0 && !isSubmitted) {
+      setTimeUp(true);
+      handleSubmitQuiz();
       return;
     }
-    if (!showTimerModal) {
+    if (!showTimerModal && !isSubmitted) {
       const interval = setInterval(() => {
         if (seconds === 0) {
           if (minutes === 0) {
@@ -51,16 +55,17 @@ const AttemptQuiz = () => {
       }, 1000);
       return () => clearInterval(interval);
     }
-  }, [seconds, minutes, showTimerModal]);
+  }, [seconds, minutes, showTimerModal, isSubmitted]);
 
   useEffect(() => {
     if (startTimer === 0) {
       setShowTimerModal(false);
+    } else {
+      const timer = setInterval(() => {
+        setStartTimer((prevStartTimer) => prevStartTimer - 1);
+      }, 1000);
+      return () => clearInterval(timer);
     }
-    const timer = setInterval(() => {
-      setStartTimer((prevStartTimer) => prevStartTimer - 1);
-    }, 1000);
-    return () => clearInterval(timer);
   }, [startTimer]);
 
   const handleNextQuestion = () => {
@@ -84,7 +89,7 @@ const AttemptQuiz = () => {
 
   const currentQuestion = questions[currentQuestionIndex];
 
-  const handelSubmitQuiz = () => {
+  const handleSubmitQuiz = () => {
     let answerSubmitted = selectedOptions;
     let correctAnswers = {};
     let tempScore = 0;
@@ -111,16 +116,28 @@ const AttemptQuiz = () => {
       <div className='flex justify-between items-center p-4'>
         <h1 className='text-5xl font-bold text-teal-600'>{quizName}</h1>
         <div className='text-xl font-bold text-center'>
-          <p>Time Limit: {time_limit} minutes</p>
+          <p>Time Limit: {timeLimit} minutes</p>
           <p className='text-red-600'>Timer: {minutes.toString().padStart(2, '0')}:{seconds.toString().padStart(2, '0')}</p>
         </div>
       </div>
       {showTimerModal && (
         <div className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-60 flex-col '>
           <h2 className='font-bold text-4xl text-white'>Quiz Starts in</h2>
-          <h1 className='text-4xl text-red-600 font-bold'>{startTimer === 0 ? 'Time\'s up!' : `${startTimer} seconds`}</h1>
+          <h1 className='text-4xl text-red-600 font-bold'>{startTimer === 0 ? 'Start!' : `${startTimer} seconds`}</h1>
         </div>
       )}
+      {timeUp && (
+        <div className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-60 flex-col '>
+          <h2 className='font-bold text-4xl text-white'>Time's up!</h2>
+          <button
+            className='border p-3 mt-4 font-bold text-white bg-blue-600 hover:bg-blue-500 rounded-md'
+            onClick={() => setTimeUp(false)}
+          >
+            Show Result
+          </button>
+        </div>
+      )}
+      
       <p className='mx-5 text-neutral-600'>{`${currentQuestionIndex + 1} / ${no_of_questions}`}</p>
       {currentQuestion && (
         <Question
@@ -135,14 +152,14 @@ const AttemptQuiz = () => {
         <button
           className='border py-2 font-bold text-white bg-teal-700 rounded-md hover:bg-teal-600 disabled:opacity-50'
           onClick={handlePreviousQuestion}
-          disabled={currentQuestionIndex === 0}
+          disabled={currentQuestionIndex === 0 || isSubmitted}
         >
           &#8592; Previous
         </button>
         <button
           className='border py-3 font-bold text-white bg-teal-700 rounded-md hover:bg-teal-600 disabled:opacity-50'
           onClick={handleNextQuestion}
-          disabled={currentQuestionIndex === questions.length - 1}
+          disabled={currentQuestionIndex === questions.length - 1 || isSubmitted}
         >
           Next &#8594;
         </button>
@@ -151,22 +168,33 @@ const AttemptQuiz = () => {
         <button
           className='border p-3 px-4 font-bold text-white bg-red-600 rounded-md hover:bg-red-500'
           onClick={() => window.location.replace('/quizzes')}
+          disabled={isSubmitted}
         >
           Cancel
         </button>
         <button
           className='border p-3 font-bold text-white bg-blue-600 hover:bg-blue-500 rounded-md disabled:opacity-50'
-          onClick={handelSubmitQuiz}
+          onClick={handleSubmitQuiz}
           disabled={isSubmitted}
         >
           Submit Quiz
         </button>
       </div>
-      {isSubmitted && (
-        <div className='mt-4 text-2xl font-bold text-green-600'>
-          Your Score: {score}/{questions.length}
+      {isSubmitted && !timeUp ? (
+        <div className='fixed inset-0 flex items-center justify-center bg-black bg-opacity-60 flex-col'>
+          <div className='bg-white p-6 px-10 rounded-lg text-center flex flex-col gap-4'>
+            <h2 className='text-4xl text-green-500 font-bold'>Congratulations</h2>
+            <p className='text-lg'>Your Score</p>
+            <p className='text-2xl font-bold'><span className='text-green-500'>{score}</span> / {no_of_questions}</p>
+            <button 
+              className='w-full p-3 bg-teal-700 text-white rounded-lg text-lg'
+              onClick={() => window.location.replace('/quizzes')}
+            >
+              Continue
+            </button>
+          </div>
         </div>
-      )}
+      ): null}
     </div>
   );
 };
